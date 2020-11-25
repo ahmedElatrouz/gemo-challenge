25b6b43948206a834bfb608cd47c7d61864916de
ahmed elatrouz <aelatrouz@gmail.com>
2020-11-25 16:25:14 +0100

    adding the rest controller

--- src/main/java/com/ahmed/gemochallenge/controllers/ReposController.java
+++ src/main/java/com/ahmed/gemochallenge/controllers/ReposController.java
@@ -1,14 +1,33 @@
 package com.ahmed.gemochallenge.controllers;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import com.ahmed.gemochallenge.entities.GitRepository;
+import com.ahmed.gemochallenge.entities.Language;
+import com.ahmed.gemochallenge.resources.ReposResource;
+import com.ahmed.gemochallenge.services.SortingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.*;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/repos")
 public class ReposController{
 
+    @Autowired
+    private SortingService sortingService;
+    @Autowired
+    private ReposResource reposResource;
 
+        @GetMapping("/languages/top100")
+        public List<Language> getSortedLanguages(){
+            return sortingService.getSortedLanguages();
+        }
+        @GetMapping("")
+        public Collection<GitRepository> getrepos(){
+            return reposResource.getData().getItems();
+        }
 }
