f6c062b62ab463e184f78620f2634cb6991fb91d
ahmed elatrouz <aelatrouz@gmail.com>
2020-11-25 16:24:54 +0100

    adding the the sorting service

--- src/main/java/com/ahmed/gemochallenge/services/SortingService.java
+++ src/main/java/com/ahmed/gemochallenge/services/SortingService.java
@@ -1,4 +1,42 @@
 package com.ahmed.gemochallenge.services;
 
+import com.ahmed.gemochallenge.entities.GitRepository;
+import com.ahmed.gemochallenge.entities.GithubApi;
+import com.ahmed.gemochallenge.entities.Language;
+import com.ahmed.gemochallenge.resources.ReposResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class SortingService {
+
+    @Autowired
+    private ReposResource reposResource;
+
+    public List<Language> getSortedLanguages(){
+        //getting the data from the resource
+        GithubApi githubApi=reposResource.getData();
+
+        //sorting extracting every language with the number of its uses
+        List<Language> languages=new ArrayList<>();
+        for (GitRepository gr : githubApi.getItems()) {
+            //if no language then skip
+            if(gr.getLanguage()==null)
+                continue;
+
+            //else create a language object (language, numberOfUses)
+            Language aLanguage=new Language(gr.getLanguage(),Collections.frequency(githubApi.getItems(),gr));
+            if(languages.contains(aLanguage))
+                continue;
+
+            languages.add(aLanguage);
+        }
+        //sorting the languages according to the number Of Uses, using the compareTo() methode
+        Collections.sort(languages);
+
+        return languages;
+    }
+
 }
